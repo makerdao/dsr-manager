ef2b7698295dcb29df875f874e1c117f37c24595
Mariano Conti <nanexcool@gmail.com>
2020-01-17 12:51:41 -0300

    Refactor

--- src/DsrManager.sol
+++ src/DsrManager.sol
@@ -41,35 +41,35 @@ contract GemLike {
 contract DsrManager {
     VatLike  public vat = VatLike(0x35D1b3F3D7966A1DFe207aa4514C12a259A0492B);
     PotLike  public pot = PotLike(0x197E90f9FAD81970bA7976f33CbD77088E5D7cf7);
+    GemLike  public dai = GemLike(0x6B175474E89094C44Da98b954EedeAC495271d0F);
     JoinLike public daiJoin = JoinLike(0x9759A6Ac90977b93B58547b4A71c78317f391A28);
-    GemLike  public daiToken = GemLike(0x6B175474E89094C44Da98b954EedeAC495271d0F);
 
-    uint256 public totalSupply;
+    uint256 public supply;
 
     mapping (address => uint256) public pieOf;
 
     event Join(address indexed dst, uint256 wad);
     event Exit(address indexed dst, uint256 wad);
 
-    uint constant RAY = 10 ** 27;
-    function add(uint x, uint y) internal pure returns (uint z) {
+    uint256 constant RAY = 10 ** 27;
+    function add(uint256 x, uint256 y) internal pure returns (uint256 z) {
         require((z = x + y) >= x);
     }
-    function sub(uint x, uint y) internal pure returns (uint z) {
+    function sub(uint256 x, uint256 y) internal pure returns (uint256 z) {
         require((z = x - y) <= x);
     }
-    function mul(uint x, uint y) internal pure returns (uint z) {
+    function mul(uint256 x, uint256 y) internal pure returns (uint256 z) {
         require(y == 0 || (z = x * y) / y == x);
     }
-    function rmul(uint x, uint y) internal pure returns (uint z) {
+    function rmul(uint256 x, uint256 y) internal pure returns (uint256 z) {
         // always rounds down
         z = mul(x, y) / RAY;
     }
-    function rdiv(uint x, uint y) internal pure returns (uint z) {
+    function rdiv(uint256 x, uint256 y) internal pure returns (uint256 z) {
         // always rounds down
         z = mul(x, RAY) / y;
     }
-    function rdivup(uint x, uint y) internal pure returns (uint z) {
+    function rdivup(uint256 x, uint256 y) internal pure returns (uint256 z) {
         // always rounds up
         z = add(mul(x, RAY), sub(y, 1)) / y;
     }
@@ -78,36 +78,36 @@ contract DsrManager {
         vat.hope(address(daiJoin));
         vat.hope(address(pot));
 
-        daiToken.approve(address(daiJoin), uint(-1));
+        dai.approve(address(daiJoin), uint256(-1));
     }
 
-    function dai(address usr) external returns (uint wad) {
-        uint chi = (now > pot.rho()) ? pot.drip() : pot.chi();
+    function daiBalance(address usr) external returns (uint256 wad) {
+        uint256 chi = (now > pot.rho()) ? pot.drip() : pot.chi();
         wad = rmul(chi, pieOf[usr]);
     }
 
     // wad is denominated in dai
-    function join(address dst, uint wad) external {
-        uint chi = (now > pot.rho()) ? pot.drip() : pot.chi();
-        uint pie = rdiv(wad, chi);
+    function join(address dst, uint256 wad) external {
+        uint256 chi = (now > pot.rho()) ? pot.drip() : pot.chi();
+        uint256 pie = rdiv(wad, chi);
         pieOf[dst] = add(pieOf[dst], pie);
-        totalSupply    = add(totalSupply, pie);
+        supply = add(supply, pie);
 
-        daiToken.transferFrom(msg.sender, address(this), wad);
+        dai.transferFrom(msg.sender, address(this), wad);
         daiJoin.join(address(this), wad);
         pot.join(pie);
         emit Join(dst, wad);
     }
 
     // wad is denominated in dai
-    function exit(address dst, uint wad) external {
-        uint chi = (now > pot.rho()) ? pot.drip() : pot.chi();
-        uint pie = rdivup(wad, chi);
+    function exit(address dst, uint256 wad) external {
+        uint256 chi = (now > pot.rho()) ? pot.drip() : pot.chi();
+        uint256 pie = rdivup(wad, chi);
 
         require(pieOf[msg.sender] >= pie, "insufficient-balance");
 
         pieOf[msg.sender] = sub(pieOf[msg.sender], pie);
-        totalSupply = sub(totalSupply, pie);
+        supply = sub(supply, pie);
 
         pot.exit(pie);
         uint256 amt = rmul(chi, pie);
@@ -116,11 +116,11 @@ contract DsrManager {
     }
 
     function exitAll(address dst) external {
-        uint chi = (now > pot.rho()) ? pot.drip() : pot.chi();
-        uint pie = pieOf[msg.sender];
+        uint256 chi = (now > pot.rho()) ? pot.drip() : pot.chi();
+        uint256 pie = pieOf[msg.sender];
 
         pieOf[msg.sender] = 0;
-        totalSupply = sub(totalSupply, pie);
+        supply = sub(supply, pie);
 
         pot.exit(pie);
         uint256 amt = rmul(chi, pie);
